aeef844f74d8865d2f6423b71612a831e3a30d44
moogiikat-gtn <b.munkh@gtn.co.jp>
2023-07-02 22:10:47 +0800

    create next project

---
+++ portfolio/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className="mockup-window border bg-base-300">
+      <div className="flex justify-center px-4 py-16 bg-base-200">Hello!</div>
+    </div>
+  )
+}
---
+++ portfolio/tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [require("daisyui"), require("@tailwindcss/line-clamp")],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/colors/themes")["[data-theme=light]"],
+          primary: "#202020",
+          secondary: "#ff781f",
+          success: "#4caf50",
+          warning: "#e6a23b",
+          error: "#f56c6b",
+        },
+      },
+    ],
+  },
+};
