2955d2ae63edb14ea249251468117bb96ef79b69
moogiikat-gtn <b.munkh@gtn.co.jp>
2023-07-02 23:31:55 +0800

    refactor

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -3,7 +3,7 @@ import { classNames } from "lib/utils";
 export default function Home() {
   return (
     <div className="mockup-window border bg-base-300">
-      <div className="flex justify-center px-4 py-16 bg-base-200">
+      <div className="flex flex-col justify-center px-4 py-16 bg-base-200 gap-y-10">
         <div
           className={classNames("grid gap-y-5", "lg:grid-cols-2 lg:gap-x-10")}
         >
@@ -12,7 +12,7 @@ export default function Home() {
             className="w-full h-full rounded"
           ></img>
           <div className="overflow-x-auto">
-            <h1 className="text-white text-2xl pl-5 border border-x-0 border-t-0">
+            <h1 className="text-white text-2xl pl-5 border border-x-0 border-t-0 p-2">
               私について
             </h1>
             <table className="table text-white">
@@ -27,6 +27,41 @@ export default function Home() {
                 })}
               </tbody>
             </table>
+          </div>
+        </div>
+        <div className="flex flex-col gap-y-5 text-white">
+          <h1 className="text-2xl">スキル</h1>
+          <div
+            className={classNames("grid gap-y-5", "lg:grid-cols-2 lg:gap-x-10")}
+          >
+            <div className="flex flex-col gap-y-5">
+              {skills.map((item) => {
+                return (
+                  <div className="flex flex-col gap-y-2">
+                    <p>{item.label}</p>
+                    <progress
+                      className="progress progress-info w-full bg-gray-200"
+                      value={item.value}
+                      max="100"
+                    ></progress>
+                  </div>
+                );
+              })}
+            </div>
+            <div className="flex flex-col gap-y-5">
+              {skills.map((item) => {
+                return (
+                  <div className="flex flex-col gap-y-2">
+                    <p>{item.label}</p>
+                    <progress
+                      className="progress progress-info w-full bg-gray-200"
+                      value={item.value}
+                      max="100"
+                    ></progress>
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
@@ -44,4 +79,9 @@ const tableData = [
     label: "趣味",
     value: "バスケットボール、チェス、旅行、写真撮影、コーディング",
   },
+];
+
+const skills = [
+  { label: "css", value: "90" },
+  { label: "html", value: "90" },
 ];
