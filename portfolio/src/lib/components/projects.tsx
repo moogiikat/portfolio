7d1901432e1434824e6a28941e73b678682f47a2
moogiikat-gtn <b.munkh@gtn.co.jp>
2023-07-05 23:36:25 +0800

    add project

--- portfolio/src/lib/components/projects.tsx
+++ portfolio/src/lib/components/projects.tsx
@@ -7,6 +7,24 @@ export const Projects: React.FC = () => {
   return (
     <div className="flex flex-col gap-y-5 text-white">
       <h1 className="text-2xl font-bold">今までのプロジェクト</h1>
+      <div className="stats bg-gray-800">
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <div className="avatar online">
+              <div className="w-16 rounded-full">
+                <img src="/assets/home_image.jpg" />
+              </div>
+            </div>
+          </div>
+          <div className="stat-value text-white">プロジェクト</div>
+          <div className="stat-title text-white">
+            参加済みなプロジェクトは10個
+          </div>
+          <div className="stat-desc text-secondary">
+            現在参加しているプロジェクトは2個
+          </div>
+        </div>
+      </div>
       <div className={classNames("grid gap-y-5", "lg:gap-x-5 lg:grid-cols-3")}>
         {projects.map((item, idx) => {
           return (
